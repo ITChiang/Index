46c94803b244a50131c98b2c956f831d6d2c15b6
ITC <ex35214@gmail.com>
2019-02-08 14:04:35 +0800

    🎶Add bgm

--- Random/src/ui.js
+++ Random/src/ui.js
@@ -11,10 +11,12 @@ function playbutton(e) {
   var key = e.keyCode;
   if (key == 32 && playBool == true) {
     two.pause();
+    scriptMidi.pause();
     playBool = false;
   } else if (key == 32 && playBool == false) {
     playBool = true;
     two.play();
+    scriptMidi.play();
   }
 }
 
@@ -22,5 +24,8 @@ $(document).ready(function() {
   $("#playButton").mouseup(function() {
     $(".frontPage").css("z-index", "-5");
     $(".frontPage").css("opacity", "0");
+    scriptMidi.play();
   });
 });
+
+var scriptMidi = new Audio("assets/bgm1.mp3");
